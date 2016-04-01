0952becf6cd1a24d928a4de8c230f92faa5ef948
mjereb@gmail.com <mjereb@gmail.com>
2016-04-01 15:26:15 +0000

    github commit

--- lucyserial.py
+++ lucyserial.py
@@ -10,6 +10,7 @@ while True:
 	port.flushOutput()
 	rpi_debug = "\0x02\0x16\0x20\0x00\0x00\0x00\0x38" 
 #	rpi_debug = "\0x09"
+#	rpi_debug = ("abcdefghij")
 	port.write(rpi_debug)
         port.flush()
 	time.sleep(1)
