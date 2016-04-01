20992920dd7ffde7c5164570892fa234d015cfbb
Mitja Jereb <mjereb@gmail.com>
2016-04-01 17:41:10 +0200

    inwaiting

--- lucyserial.py
+++ lucyserial.py
@@ -5,12 +5,14 @@ port=serial.Serial("/dev/ttyAMA0", baudrate=38400, timeout=1)
 
 
 while True:
-	#rcv = port.read(10)
-#	print(rcv)
-	port.flushOutput()
-	rpi_debug = "\0x02\0x16\0x20\0x00\0x00\0x00\0x38" 
+
+	bytesToRead = port.inWaiting()
+    rcv = port.read(bytesToRead)
+    port.flushInput()
+
+	rpi_debug = "\0x02\0x16\0x20\0x00\0x00\0x00\0x38"
 #	rpi_debug = "\0x09"
 #	rpi_debug = ("abcdefghij")
 	port.write(rpi_debug)
-        port.flush()
+    port.flushOutput()
 	time.sleep(1)
